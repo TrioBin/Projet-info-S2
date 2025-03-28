04c3ae564f11e8f7bc2feec64a35b8d5b04f6f09
Ascol57 <constant.rusche@gmail.com>
2025-03-28 08:29:36 +0100

    add properties for products, operators, and goals in Workshop class

--- workshopctrl/src/main/java/fr/triobin/workshopctrl/Workshop.java
+++ workshopctrl/src/main/java/fr/triobin/workshopctrl/Workshop.java
@@ -5,4 +5,7 @@ import java.util.ArrayList;
 public class Workshop {
     private String designation;
     private ArrayList<Workstation> workstations;
+    private ArrayList<Product> products;
+    private ArrayList<Operator> operators;
+    private ArrayList<Goal> goals;
 }
